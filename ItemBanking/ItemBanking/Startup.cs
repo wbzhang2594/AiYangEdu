0a72ab74aa6c81bae37e1fe674a3ed3c5944b56d
Wenbin Zhang <ght648@motorolasolutions.com>
2015-09-24 11:31:40 +0800

    Demo Web App

---
+++ ItemBanking/ItemBanking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ItemBanking.Startup))]
+namespace ItemBanking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
